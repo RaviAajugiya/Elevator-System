f56d539d3b3845b31f428032a13dded8216a6f77
Ravi Aajugiya <raviaajugiya@weybee.in>
2023-09-13 19:17:55 +0530

    Changes to be committed: 	modified:   index.html 	modified:   script.js 	modified:   style.css 	modified:   style.css.map 	modified:   style.scss

--- script.js
+++ script.js
@@ -1,74 +1,25 @@
+let currfloor = [0,0,0,0,0]
+let floorbtn = document.querySelectorAll('.floorbtn')
 
-const sw = document.querySelectorAll(".switch");
-const onoff = document.querySelectorAll(".onoff");
-let isBtnOn = true;
+const handleAnimation = (e) => {
+    // console.log(e.target.id.slice(-1));
+    let curr = e.target.id.slice(-1)
+    console.log('curr', curr);
+    console.log(currfloor);
+    let temp = currfloor.map(c => Math.abs(curr - c));
+    console.log(temp);
 
-sw.forEach(element => {
-    element.addEventListener('click', (e) => {
-        console.log(e.target.id);
-        if(isBtnOn){
-            document.getElementById(e.target.id).src = 'icons/switch-off.png'
-            isBtnOn = false
-        }else{
-            document.getElementById(e.target.id).src = 'icons/switch-on.png'
-            isBtnOn = true
-        }  
-    })
-});
+    let min = temp.reduce((acc,curr) => {
+        return acc < curr ? acc : curr 
+    }, 0)
+    console.log(min);
+    
+    
+    let random = Math.floor(Math.random()*5);
+    currfloor[random] = Number(e.target.id.slice(-1));
+    document.querySelector(`#box${random}`).style.bottom = `${currfloor[random]}00px`;
+}
 
-
-
-
-
-
-
-// maintenance.forEach(el => {
-//     el.addEventListener('click', switchhandle)
-// })
-
-// let elevators = 4;
-// let floors = 5;
-
-// let main = document.getElementById('main');
-// let control = document.getElementById('control')
-// let currFloor;
-
-// let updateUI = () => {
-//     let htmlel = `
-//     <div class="elevatorArea">
-//         <div class="elevator">
-//             <div><span class="currFloor" >1</span></div>
-//             <div><span class="currFloor" >1</span></div>
-//             <div><span class="currFloor" >1</span></div>
-//             <div><span class="currFloor" >1</span></div>
-//             <div><span class="currFloor" >1</span></div>
-//             <div class="el">
-//             </div>
-//         </div>
-//         <div class="switch">
-//             <img src="icons/switch-off.png" alt="">
-//             <img src="icons/switch-on.png" alt="">
-//         </div>
-//     </div>`
-//     main.insertAdjacentHTML('afterbegin', htmlel);
-
-// }
-
-// for (let i = 1; i <= elevators; i++) {
-//     updateUI(i);
-// }
-
-
-// let hrmlfl = `
-// <div class="floorContainer">
-//     <div class="floor"><span>1</span></div>
-//     <div class="floor floorbtn">
-//         <img src="icons/arrow-up.png" alt="">
-//     </div>
-// </div>`
-
-// for (let i = 0; i < floors; i++) {
-//     control.insertAdjacentHTML('afterbegin', hrmlfl);
-// }
-
-
+floorbtn.forEach(f => {
+    f.addEventListener('click', handleAnimation)
+})
