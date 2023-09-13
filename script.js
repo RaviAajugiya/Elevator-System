a22598d8b2baa79651c88b41db5b0e10a32710c3
Aajugiya Ravi <aajugiyaravi@gmail.com>
2023-09-14 00:47:00 +0530

    dynamic html

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
 let floorNum = 10;
 let elevator = 3;
 
+for (let i = 0; i < floorNum; i++) {
+    let elcontainerHTML = `
+        <div><span>${i}</span></div>
+    `
+    let floorHTML = `
+        <div class="floor">
+            <span>${i}</span>
+            <img id="floor${i}" class="floorbtndown floorbtnup" src="icons/arrow-up.png" alt="">
+        </div>
+    `;
+    document.querySelector(`.elcontainer`).insertAdjacentHTML('afterbegin',elcontainerHTML)
+    document.querySelector(`.floors`).insertAdjacentHTML('afterbegin',floorHTML)
+}
+for (let i = 0; i < elevator; i++) {
+    let switchHTML = `
+        <div>
+            <img src="icons/switch-on.png" alt="" class="switch" id="switch${i}">
+        </div>
+    `;
+    let elevatorHTML = `
+        <div class="elevator" id="elevator${i}">
+            <div>
+                <div><span>0</span></div>
+            </div>
+            <div class="box" id="box${i}"></div>
+        </div>
+    `;
+
+    document.querySelector(`.elcontainer`).insertAdjacentHTML('afterbegin',elcontainerHTML)
+    document.querySelector(`.floors`).insertAdjacentHTML('afterbegin',floorHTML)
+}
 
 
 let currfloor = Array(elevator).fill(0);
