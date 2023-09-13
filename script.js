3cdab8609f982ef65e9d00832ffbf160655141c6
Aajugiya Ravi <aajugiyaravi@gmail.com>
2023-09-14 00:25:09 +0530

    EL v 1.0

--- script.js
+++ script.js
@@ -1,25 +1,69 @@
-let currfloor = [0,0,0,0,0]
-let floorbtn = document.querySelectorAll('.floorbtn')
+let floorNum = 10;
+let elevator = 3;
+
+
+
+let currfloor = Array(elevator).fill(0);
+console.log(currfloor);
+let floorbtn = document.querySelectorAll('.floorbtnup')
 
 const handleAnimation = (e) => {
-    // console.log(e.target.id.slice(-1));
     let curr = e.target.id.slice(-1)
-    console.log('curr', curr);
-    console.log(currfloor);
-    let temp = currfloor.map(c => Math.abs(curr - c));
-    console.log(temp);
+    let temp = currfloor.map(c =>
+        Math.abs(curr - c)
+        // curr - c
+    );
+    console.log('temp', temp);
 
-    let min = temp.reduce((acc,curr) => {
-        return acc < curr ? acc : curr 
-    }, 0)
-    console.log(min);
-    
-    
-    let random = Math.floor(Math.random()*5);
-    currfloor[random] = Number(e.target.id.slice(-1));
-    document.querySelector(`#box${random}`).style.bottom = `${currfloor[random]}00px`;
+    let min = temp.reduce((acc, curr) => {
+        return acc < curr ? acc : curr
+    })
+    console.log('min', min);
+    let indexar = [];
+    temp.forEach((c, i) => {
+        if (c == min) {
+            indexar.push(i)
+        }
+    })
+    console.log('i', indexar);
+    if (min != 0) {
+        let random = indexar[Math.floor(Math.random() * indexar.length)];
+        currfloor[random] = Number(e.target.id.slice(-1));
+        console.log('random', random);
+        document.querySelector(`#box${random}`).style.bottom = `${currfloor[random]}00px`;
+    }
+    console.log(currfloor);
 }
 
 floorbtn.forEach(f => {
     f.addEventListener('click', handleAnimation)
 })
+
+
+
+let imgs = document.querySelectorAll('.switch');
+imgs.forEach(i => {
+    let on = true;
+    i.addEventListener('click', e => {
+        console.log(e.target.id);
+        if (on) {
+            document.querySelector(`#${e.target.id}`).src = 'icons/switch-off.png';
+            document.querySelector(`#box${e.target.id.slice(-1) - 1}`).style.border = `3px solid red`;
+            console.log(currfloor);
+            //go to down on off
+            document.querySelector(`#box${e.target.id.slice(-1) - 1}`).style.bottom = `0px`;
+
+            delete currfloor[e.target.id.slice(-1) - 1]
+            console.log(currfloor);
+            on = false;
+
+        } else {
+            document.querySelector(`#${e.target.id}`).src = 'icons/switch-on.png';
+            document.querySelector(`#box${e.target.id.slice(-1) - 1}`).style.border = `none`;
+            currfloor[e.target.id.slice(-1) - 1] = 0;
+            on = true;
+
+        }
+    })
+})
+console.log(imgs);
